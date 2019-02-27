921b7e08463a06e096138ba2f8fe4de3a89e8739
HelenSolovey <47430048+HelenSolovey@users.noreply.github.com>
2019-02-27 22:42:05 +0300

    body function

--- PadawansTask7/StringExtension.cs
+++ PadawansTask7/StringExtension.cs
@@ -4,10 +4,29 @@ namespace PadawansTask7
 {
     public class StringExtension
     {
+        
         public static void OrderStringsByLength(string[] array)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (array == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+
+            for (int j = 0; j < array.Length; j++)
+            {
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    string tmp;
+                    if (array[i].Length >= array[i + 1].Length)
+                    {
+                        tmp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = tmp;
+
+                    }
+                }
+            }
         }
     }
 }
